596739a8dd969f31c5b7cb0e5a9c35f3bca80f20
Mohammed Adib <76277079+ADIB-the-NOOB@users.noreply.github.com>
2022-08-21 02:24:10 +0600

    Total Votes

--- posts/serializers.py
+++ posts/serializers.py
@@ -4,10 +4,13 @@ from rest_framework import serializers
 class PostSerializer(serializers.ModelSerializer):
     poster = serializers.ReadOnlyField(source='poster.username')
     poster_id = serializers.ReadOnlyField(source='poster.id')
+    votes = serializers.SerializerMethodField()
     class Meta:
         model = Post
-        fields = ('title', 'url', 'poster', 'poster_id','created_at',)
+        fields = ('title', 'url', 'poster', 'poster_id','votes','created_at',)
 
+    def get_votes(self, post):
+        return Vote.objects.filter(post=post).count()
 class VoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vote
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .serializers import PostSerializer,VoteSerializer
 from .models import Post, Vote
-from rest_framework import exceptions
+from rest_framework import exceptions,mixins,response,status
 from rest_framework import generics, permissions
 # Create your views here.
 
@@ -13,7 +13,7 @@ class PostList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(poster=self.request.user)
 
-class VoteCreate(generics.CreateAPIView):
+class VoteCreate(generics.CreateAPIView,mixins.DestroyModelMixin):
     serializer_class = VoteSerializer
     permission_classes = (permissions.IsAuthenticated,)
      
@@ -26,3 +26,10 @@ class VoteCreate(generics.CreateAPIView):
         if self.get_queryset().exists():
             raise exceptions.ValidationError("You have already voted for this post")
         serializer.save(voter=self.request.user,post=Post.objects.get(pk=self.kwargs['pk']))
+
+    def delete(self, request, *args, **kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return response.Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise exceptions.ValidationError("You have not voted for this post")
